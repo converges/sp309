3c62dd5c18285f25277637473a34c4d07ffc365f
Anu <mindtheorigin@gmail.com>
2023-04-25 01:55:13 +0900

    week5 daemon-ex

--- week5/fptr-ex.c
+++ week5/fptr-ex.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+void greet();
+void caller(void (*myFunction) (void));
+
+int main(void) {
+
+    void (*sayHello) (void);
+
+    printf("Calling greet() directly.\n");
+    greet();
+
+    printf("\n");
+
+    printf("Calling greet() via a function pointer.\n");
+    sayHello = greet;
+    sayHello();
+    printf("\n");
+
+    printf("Calling greet() via another function.\n");
+    caller(greet);
+
+    return 0;
+}
+
+void greet() {
+    printf("Hello, world!\n");
+}
+
+void caller(void (*myFunction) (void)) {
+    myFunction();
+}
