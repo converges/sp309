84fd7bcebe0ce20af72c242963db73bd3715ae0e
converges <mindtheorigin@gmail.com>
2023-05-04 00:34:15 +0000

    Bug fix

--- week10/semaphore.c
+++ week10/semaphore.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     pthread_create(&thread1, NULL, Thread1, NULL);
-    phtread_create(&thread2, NULL, Thread2, NULL);
+    pthread_create(&thread2, NULL, Thread2, NULL);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     printf("%d\n", cnt);
